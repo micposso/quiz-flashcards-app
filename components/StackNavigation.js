b0be9ddcb1fb8e47b0ec49669ce2e82173845b81
unknown <micposso@gmail.com>
2019-04-26 00:09:23 -0400

    add custom icon to stackedNavigation

--- components/StackNavigation.js
+++ components/StackNavigation.js
@@ -1,55 +1,43 @@
 import React from "react";
+import { TouchableHighlight } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Navigation from "./Navigation";
 import Deck from "./Deck";
 import AddCard from "./AddCard";
 import Quiz from "./Quiz";
-import { Entypo } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 import { appStyles, colors } from "../utils/Styles";
 
 const Back = ({ onPress }) => (
   <TouchableHighlight onPress={onPress}>
-    <Entypo name="back" />
+    <AntDesign name="leftcircle" color="white" size={30} style={{ marginLeft: 20 }} />
   </TouchableHighlight>
 );
 
-const StackMenu = createStackNavigator({
-  Main: {
-    screen: Navigation,
-    navigationOptions: {
+const StackMenu = createStackNavigator(
+  {
+    Main: {
+      screen: Navigation
+    },
+    Deck: {
+      screen: Deck
+    },
+    AddCard: {
+      screen: AddCard
+    },
+    Quiz: {
+      screen: Quiz
+    }
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: "white",
+      headerLeft: <Back />,
       headerStyle: {
-        backgroundColor: colors.primaryBlue
+        backgroundColor: colors.headerColor
       }
     }
-  },
-  Deck: {
-    screen: Deck,
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: colors.primaryBlue
-      },
-    }
-  },
-  AddCard: {
-    screen: AddCard,
-    navigationOptions: {
-      title: "Add Card",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: colors.primaryBlue
-      },
-    }
-  },
-  Quiz: {
-    screen: Quiz,
-    navigationOptions: {
-      title: "Quiz",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: colors.primaryBlue
-      },
-    }
   }
-});
+);
 
 export default createAppContainer(StackMenu);
